90956d6a6543e8c235aeb90c49d140b015214e87
Dan Schaper <dan@glacialmagma.com>
2018-01-11 22:33:48 -0800

    Savepoint

---
+++ .config/zsh/functs.zsh
@@ -0,0 +1,161 @@
+# intellegently extract archives based on extension. 
+extract() {
+	local remove_archive
+	local success
+	local extract_dir
+
+	if (( $# == 0 )); then
+		cat <<-'EOF' >&2
+			Usage: extract [-option] [file ...]
+			Options:
+			    -r, --remove    Remove archive.
+		EOF
+	fi
+
+	remove_archive=1
+	if [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
+		remove_archive=0
+		shift
+	fi
+
+	while (( $# > 0 )); do
+		if [[ ! -f "$1" ]]; then
+			echo "extract: '$1' is not a valid file" >&2
+			shift
+			continue
+		fi
+
+		success=0
+		extract_dir="${1:t:r}"
+		case "$1" in
+			(*.tar.gz|*.tgz) (( $+commands[pigz] )) && { pigz -dc "$1" | tar xv } || tar zxvf "$1" ;;
+			(*.tar.bz2|*.tbz|*.tbz2) tar xvjf "$1" ;;
+			(*.tar.xz|*.txz)
+				tar --xz --help &> /dev/null \
+				&& tar --xz -xvf "$1" \
+				|| xzcat "$1" | tar xvf - ;;
+			(*.tar.zma|*.tlz)
+				tar --lzma --help &> /dev/null \
+				&& tar --lzma -xvf "$1" \
+				|| lzcat "$1" | tar xvf - ;;
+			(*.tar) tar xvf "$1" ;;
+			(*.gz) (( $+commands[pigz] )) && pigz -d "$1" || gunzip "$1" ;;
+			(*.bz2) bunzip2 "$1" ;;
+			(*.xz) unxz "$1" ;;
+			(*.lzma) unlzma "$1" ;;
+			(*.Z) uncompress "$1" ;;
+			(*.zip|*.war|*.jar|*.sublime-package|*.ipsw|*.xpi|*.apk) unzip "$1" -d $extract_dir ;;
+			(*.rar) unrar x -ad "$1" ;;
+			(*.7z) 7za x "$1" ;;
+			(*.deb)
+				mkdir -p "$extract_dir/control"
+				mkdir -p "$extract_dir/data"
+				cd "$extract_dir"; ar vx "../${1}" > /dev/null
+				cd control; tar xzvf ../control.tar.gz
+				cd ../data; extract ../data.tar.*
+				cd ..; rm *.tar.* debian-binary
+				cd ..
+			;;
+			(*)
+				echo "extract: '$1' cannot be extracted" >&2
+				success=1
+			;;
+		esac
+
+		(( success = $success > 0 ? $success : $? ))
+		(( $success == 0 )) && (( $remove_archive == 0 )) && rm "$1"
+		shift
+	done
+}
+
+# Make a directory and cd into it
+function take {
+	mkdir -p $1
+	cd $1
+}
+
+# web_search from terminal
+function web_search() {
+  emulate -L zsh
+
+  # define search engine URLS
+  typeset -A urls
+  urls=(
+    google      "https://www.google.com/search?q="
+    ddg         "https://www.duckduckgo.com/?q="
+    github      "https://github.com/search?q="
+  )
+
+  # check whether the search engine is supported
+  if [[ -z "$urls[$1]" ]]; then
+    echo "Search engine $1 not supported."
+    return 1
+  fi
+
+  # search or go to main page depending on number of arguments passed
+  if [[ $# -gt 1 ]]; then
+    # build search url:
+    # join arguments passed with '+', then append to search engine URL
+    url="${urls[$1]}${(j:+:)@[2,-1]}"
+  else
+    # build main page url:
+    # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
+    url="${(j://:)${(s:/:)urls[$1]}[1,2]}"
+  fi
+
+  open_command "$url"
+}
+
+#use generalized open command
+function open_command() {
+  emulate -L zsh
+  setopt shwordsplit
+
+  local open_cmd
+
+  # define the open command
+  case "$OSTYPE" in
+    darwin*)  open_cmd='open' ;;
+    cygwin*)  open_cmd='cygstart' ;;
+    linux*)   open_cmd='xdg-open' ;;
+    msys*)    open_cmd='start ""' ;;
+    *)        echo "Platform $OSTYPE not supported"
+              return 1
+              ;;
+  esac
+
+  # don't use nohup on OSX
+  if [[ "$OSTYPE" == darwin* ]]; then
+    $open_cmd "$@" &>/dev/null
+  else
+    nohup $open_cmd "$@" &>/dev/null
+  fi
+}
+
+# Show dots while waiting for tab-completion
+expand-or-complete-with-dots() {
+	# toggle line-wrapping off and back on again
+	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti rmam
+	print -Pn "%{%F{red}......%f%}"
+	[[ -n "$terminfo[rmam]" && -n "$terminfo[smam]" ]] && echoti smam
+
+	zle expand-or-complete
+	zle redisplay
+}
+zle -N expand-or-complete-with-dots
+
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="$EDITOR $LBUFFER"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
--- .zshrc
+++ .zshrc
@@ -1,11 +1,14 @@
 # vim:ft=zsh ts=2 sw=2 sts=2 et fenc=utf-8
 # With a little help from www.joshuad.net/zshrc-config
 # Files and Directories
-local ZSH_CONF=$HOME/.zsh				# Define the place to store everything
-local ZSH_CACHE=$ZSH_CONF/cache				# Cache for storing history and zcompdump
-local LOCAL_ZSHRC=$HOME/.zshlocal/.zshrc		# Local machine overrides
+local ZSH_CONF=$XDG_CONFIG_HOME/zsh				# Define the place to store everything
+local ZSH_CACHE=$XDG_CACHE_HOME/zsh				# Cache for storing history and zcompdump
 
 export TERM="xterm-256color"
+
+if command -v nvim >/dev/null 2>&1; then
+  export EDITOR=nvim
+fi
 
 # Source helpers
   source $ZSH_CONF/functs.zsh
@@ -15,7 +18,11 @@ export TERM="xterm-256color"
 
 # Powerlevel9k theme
   source $ZSH_CONF/powerlevel9k-config.zsh
-  source $ZSH_CONF/powerlevel9k/powerlevel9k.zsh-theme
+  if [[ ! -d $XDG_DATA_HOME/powerlevel9k ]]; then
+    wget -qO - https://api.github.com/repos/bhilburn/powerlevel9k/tarball | \
+      tar -xz --wildcards --no-anchored '*theme' --strip-components=1
+  fi
+  source $XDG_DATA_HOME/powerlevel9k/powerlevel9k.zsh-theme
 
 autoload -Uz compinit promptinit
 # Load command line completions
@@ -32,7 +39,7 @@ setopt COMPLETE_ALIASES
 # Force vi mode
 bindkey -e
 # Overrides for vi mode
-bindkey "[[R" history-incremental-pattern-search-backward
+bindkey "" history-incremental-pattern-search-backward #[CTRL-R]
 
 # History
 HISTFILE=$ZSH_CACHE/history
@@ -44,5 +51,5 @@ autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-[[ -n "$key[Up]"   ]] && bindkey -- "$key[Up]"   up-line-or-beginning-search
-[[ -n "$key[Down]" ]] && bindkey -- "$key[Down]" down-line-or-beginning-search
+[[ -n "<Up>"   ]] && bindkey -- "<Up>"   up-line-or-beginning-search
+[[ -n "<Down>" ]] && bindkey -- "<Down>" down-line-or-beginning-search
